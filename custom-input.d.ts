3d7c32e64b52074f9caf8c47e0e26ec5715bb336
Gianluca Candiotti <gianluca.prwlr@gmail.com>
2021-12-03 03:27:56 +0200

    build component with typescript

---
+++ custom-input.d.ts
@@ -0,0 +1,24 @@
+import { LitElement } from "lit";
+/**
+ * An example custom input element.
+ *
+ * @slot label - The input's label.
+ * @fires input - Emitted when the control's value changes.
+ * @fires clear - Emitted when the clear button is activated.
+ * @csspart label - The input's label.
+ */
+export declare class CustomInput extends LitElement {
+    static styles: import("lit").CSSResult;
+    input: HTMLInputElement;
+    /** The input's value attribute. */
+    value: string;
+    handleInput(): void;
+    handleClearClick(): void;
+    render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "custom-input": CustomInput;
+    }
+}
+//# sourceMappingURL=custom-input.d.ts.map
---
+++ custom-input.js
@@ -0,0 +1,75 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { LitElement, html, css } from "lit";
+import { customElement, property, query } from "lit/decorators.js";
+/**
+ * An example custom input element.
+ *
+ * @slot label - The input's label.
+ * @fires input - Emitted when the control's value changes.
+ * @fires clear - Emitted when the clear button is activated.
+ * @csspart label - The input's label.
+ */
+let CustomInput = class CustomInput extends LitElement {
+    constructor() {
+        super(...arguments);
+        /** The input's value attribute. */
+        this.value = "";
+    }
+    handleInput() {
+        this.value = this.input.value;
+        this.dispatchEvent(new CustomEvent("value-changed"));
+    }
+    handleClearClick() {
+        this.value = "";
+    }
+    render() {
+        return html `
+      <label part="label" for="custom-input-control">
+        <slot name="label"></slot>
+      </label>
+
+      <div class="input-wrapper">
+        <input
+          id="custom-input-control"
+          .value=${this.value}
+          @input=${this.handleInput}
+        />
+        <button @click=${this.handleClearClick}>Clear Value</button>
+      </div>
+    `;
+    }
+};
+CustomInput.styles = css `
+    :host {
+      --height: 40px;
+
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+    }
+
+    .input-wrapper {
+      display: flex;
+      gap: 6px;
+    }
+
+    input {
+      height: var(--height);
+    }
+  `;
+__decorate([
+    query("#custom-input-control")
+], CustomInput.prototype, "input", void 0);
+__decorate([
+    property()
+], CustomInput.prototype, "value", void 0);
+CustomInput = __decorate([
+    customElement("custom-input")
+], CustomInput);
+export { CustomInput };
+//# sourceMappingURL=custom-input.js.map
